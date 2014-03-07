e96cfaa08f2f221df9a99a48e8e315abd8c0418d
Mars <mars@substantial.com>
2014-03-07 10:41:56 -0800

    Prototype Google Calendar "Upcoming" widget.

---
+++ app/components/upcoming-events.js
@@ -0,0 +1,24 @@
+import DashboardWidgetComponent from 'appkit/components/dashboard-widget';
+
+var UpcomingEventsComponent = DashboardWidgetComponent.extend({
+
+  init: function() {
+    this._super();
+    this.set("contents", Ember.A());
+  },
+
+  actions: {
+    receiveEvent: function(data) {
+      var items = Ember.A(JSON.parse(data));
+      var contents = this.get("contents");
+      contents.clear();
+      items.slice(0,5).forEach(function(item) {
+        Ember.Logger.info(item);
+        contents.pushObject(Ember.Object.create(item));
+      });
+    }
+  }
+
+});
+
+export default UpcomingEventsComponent;
