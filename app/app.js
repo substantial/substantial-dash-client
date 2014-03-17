53706c125fa54cedf2d760f4b0f35f961ec210e5
Mars <mars@substantial.com>
2014-03-17 12:23:06 -0700

    Persist user auth w/ document.cookies

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import Resolver from 'ember/resolver';
+import Cookies from 'appkit/utils/cookies';
 
 var App = Ember.Application.extend({
   LOG_ACTIVE_GENERATION: true,
@@ -15,13 +16,22 @@ App.initializer({
 
   initialize: function(container, application) {
     var location = window.location;
-    var url = purl(location.href);
+    var url = window.purl(location.href);
     var apiKey = url.param('api_key');
+    var userName = url.param('user_name');
+
+    if (Ember.isEmpty(apiKey)) {
+      apiKey = Cookies.getItem('api_key');
+      userName = Cookies.getItem('user_name');
+    }
+
     if (!Ember.isEmpty(apiKey)) {
       var auth = Ember.Object.create({
         apiKey: apiKey,
-        name: url.param('user_name')
+        userName: userName
       });
+      Cookies.setItem('api_key', apiKey);
+      Cookies.setItem('user_name', userName);
       application.register('auth:main', auth, { instantiate: false });
       application.inject('controller', 'auth', 'auth:main');
       // Clean the auth info querystring from the document's URL.
