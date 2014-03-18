57b68587faf6ec3a7f8f280f5ebe51903b561339
Alex Novosad <alexn@substantial.com>
2014-03-18 00:36:08 -0700

    Beer widget!

---
+++ app/components/beer-at-we-work.js
@@ -0,0 +1,23 @@
+import DashboardWidgetComponent from 'appkit/components/dashboard-widget';
+
+var BeerAtWeWorkComponent = DashboardWidgetComponent.extend({
+
+  init: function() {
+    this._super();
+    this.set("contents", []);
+  },
+
+  actions: {
+    receiveEvent: function(data) {
+      var items = Ember.A(JSON.parse(data));
+      var contents = this.get("contents");
+      contents.clear();
+      items.forEach(function(item) {       
+        contents.pushObject(Ember.Object.create(item));
+      });
+    }
+  }
+
+});
+
+export default BeerAtWeWorkComponent;
