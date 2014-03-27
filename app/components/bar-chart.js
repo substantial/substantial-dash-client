e80fc8f85ec3c01a1a5d03e88a36d36eee0541ca
Mars <mars@substantial.com>
2014-03-26 17:02:20 -0700

    Shiny, dynamic Pipedrive "Sales" chart.

--- app/components/bar-chart.js
+++ app/components/bar-chart.js
@@ -1,39 +1,27 @@
 var BarChartComponent = Ember.Component.extend({
     tagName: 'svg',
-    attributeBindings: 'width height'.w(),
-    margin: {top: 20, right: 20, bottom: 30, left: 65},
-    
-    w: function(){
-      return this.get('width') - this.get('margin.left') - this.get('margin.right');
-    }.property('width'),
+    classNames: ['bar-chart'],
   
-    h: function(){
-      return this.get('height') - this.get('margin.top') - this.get('margin.bottom');
-    }.property('height'),  
-  
-    transformG: function(){
-      return "translate(" + this.get('margin.left') + "," + this.get('margin.top') + ")";
-    }.property(),
-      
-    transformX: function(){
-      return "translate(0,"+ this.get('h') +")";
-    }.property('h'),   
-
-    yAxisTitle: function(){
-      return this.get('yAxisTitle');
-    }.property('yAxisTitle'),
-  
-    draw: function(){
+    draw: function() {
       var data = this.get('data');
+      if (Ember.isEmpty(data)) { return; }
+
+      var el = this.get('element');
+      var width = parseInt(d3.select(el).style('width'), 10);
+      var height = parseInt(d3.select(el).style('height'), 10);
+      var headerSpace = 20;
+      var xAxisSpace = 50;
+      var yAxisSpace = 50;
+      var barsWidth = width - yAxisSpace;
+      var barsHeight = height - headerSpace - xAxisSpace;
 
       var yFormat = window.d3.format(".2s");
-      var width = this.get('w');
-      var height = this.get('h');
-      var svg = window.d3.select('#'+this.get('elementId'));
-      var x0 = window.d3.scale.ordinal().rangeRoundBands([0, width], 0.1);
+
+      var svg = window.d3.select(el);
+      var x0 = window.d3.scale.ordinal().rangeRoundBands([0, barsWidth], 0.1);
       var x1 = window.d3.scale.ordinal();
-      var y = window.d3.scale.linear().range([height, 0]);
-      var color = window.d3.scale.ordinal().range(["#aa1d2a", "white"]);
+      var y = window.d3.scale.linear().range([barsHeight, 0]);
+      var color = window.d3.scale.ordinal().range(["#999", "white"]);
 
       var xAxis = window.d3.svg.axis().scale(x0).orient("bottom");
       var yAxis = window.d3.svg.axis().scale(y).orient("left").tickFormat(yFormat);
@@ -46,73 +34,79 @@ var BarChartComponent = Ember.Component.extend({
       });
       y.domain([0, maxY]);
 
-      Ember.Logger.debug("max y: ", maxY);
-
-      svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
+      svg.select(".x.axis")
+        .attr("transform", "translate("+yAxisSpace+","+(headerSpace+barsHeight)+")")
         .call(xAxis);
 
-      svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
-        .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".71em")
-          .style("text-anchor", "end")
-          .text(this.get("yAxisTitle"));
+      svg.selectAll(".x.axis text")
+        .style("text-anchor", "start")
+        .attr("transform", "translate(-20,0) rotate(22)");
 
+      var yAxisNode = svg.select(".y.axis");
+      svg.select(".y.axis .axis-title")
+        .attr("transform", "translate("+(-yAxisSpace)+",-5)");
+      yAxisNode.attr("transform", "translate("+yAxisSpace+","+headerSpace+")");
+      yAxisNode.call(yAxis);
+
+      // Grouped bars
       var group = svg.selectAll(".group")
-          .data(data)
-        .enter().append("g")
-          .attr("class", "g")
-          .attr("transform", function(d) { return "translate(" + x0(d.group) + ",0)"; });
+          .data(data, function(d) { return d.group; });
+      
+      group.enter().append("g").attr("class", "group");
 
-      group.selectAll("rect")
-          .data(function(d) { return d.values; })
-        .enter().append("rect")
-          .attr("width", x1.rangeBand())
+      group.attr("transform", function(d) { 
+          var xOffset = x0(d.group) + yAxisSpace;
+          return "translate(" + xOffset + ",0)"; 
+        });
+
+      // Main bars
+      var bars = group.selectAll("rect")
+          .data(function(d) { return d.values; });
+
+      bars.enter().append("rect");
+
+      bars.attr("width", x1.rangeBand())
+          .attr("rx", 6)
+          .attr("yx", 6)
           .attr("x", function(d) { return x1(d.legend); })
-          .attr("y", function(d) { return y(d.yValue); })
-          .attr("height", function(d) { return height - y(d.yValue); })
+          .attr("y", function(d) { return headerSpace + y(d.yValue); })
+          .attr("height", function(d) { return barsHeight - y(d.yValue); })
           .style("fill", function(d) { return color(d.legend); });
 
-      var legend = svg.selectAll(".legend")
-          .data(legendNames.slice().reverse())
-        .enter().append("g")
-          .attr("class", "legend")
-          .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+      bars.exit().remove();
 
-      legend.append("rect")
-          .attr("x", width - 18)
-          .attr("width", 18)
-          .attr("height", 18)
+      // Legend rendering
+      var legend = svg.selectAll(".legend")
+          .data(legendNames.slice());
+
+      var legendEnterGroup = legend.enter().append("g");
+      legendEnterGroup.attr("class", "legend");
+      legendEnterGroup.append("rect");
+      legendEnterGroup.append("text");
+
+      legend.attr("transform", function(d, i) { 
+        return "translate(" + i * 100 + ", 0)";
+      });
+
+      legend.selectAll("rect")
+          .attr("rx", 6)
+          .attr("yx", 6)
+          .attr("x", yAxisSpace)
+          .attr("width", 16)
+          .attr("height", 16)
           .style("fill", color);
 
-      legend.append("text")
-          .attr("x", width - 24)
-          .attr("y", 9)
-          .attr("dy", ".35em")
-          .style("text-anchor", "end")
+      legend.selectAll("text")
+          .attr("x", yAxisSpace+20)
+          .attr("dy", "1em")
+          .style("text-anchor", "start")
           .text(function(d) { return d; });
 
-      // svg.select(".axis.x").call(xAxis);
-      // svg.select(".axis.y").call(yAxis);
-  
-      // svg.select(".rects").selectAll("rect")
-      //   .data(data)
-      // .enter().append("rect")
-      //   .attr("class", "bar")
-      //   .attr("x", function(d) { return x(d.x); })
-      //   .attr("width", x.rangeBand())
-      //   .attr("y", function(d) { return y(d.y); })
-      //   .attr("height", function(d) { return height - y(d.y); });
-    }.observes("data.@each")
-  
-    // didInsertElement: function(){
-    //   this.draw();
-    // }
+    }.observes("data.@each"),
+
+    didInsertElement: function() {
+      window.d3.select(window).on('resize', this.draw.bind(this));
+    }
   });
 
 export default BarChartComponent;
