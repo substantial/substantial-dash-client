b1199c853f9e303440fc87f6537825526214bb7a
Mars <mars@substantial.com>
2014-03-31 10:03:30 -0700

    Default production build to a relative API URL.

--- config/environments/production.js
+++ config/environments/production.js
@@ -5,4 +5,9 @@
 //
 // window.ENV.public_key = '123456'
 
+// Serve the client relatively, from the server's public directory
+// (run `grunt server:dist`& copy dist/ into server)
+window.ENV.api_base_url = '';
+// ...or use a fully-qualified URL for the API.
 // window.ENV.api_base_url = 'https://dash-api.my-amazing-company.com'
+
