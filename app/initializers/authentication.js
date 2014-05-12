1c259e30f116ced6f636f00d749faa7866bca811
Mars <mars@substantial.com>
2014-05-11 18:22:59 -0700

    Implement "Logout" auth flow.

--- app/initializers/authentication.js
+++ app/initializers/authentication.js
@@ -6,30 +6,43 @@ var AuthenticationInitializer = {
   initialize: function(container, application) {
     var location = window.location;
     var url = window.purl(location.href);
-    var apiKey = url.param('api_key');
-    var userName = url.param('user_name');
+    var isLogout = url.param('logout');
     
-    if (Ember.isEmpty(apiKey)) {
-      apiKey = Cookies.getItem('api_key');
-      userName = Cookies.getItem('user_name');
+    if (isLogout === 'true') {
+      Cookies.removeItem('api_key');
+      Cookies.removeItem('user_name');
+      Ember.Logger.debug('App.initializer authentication logged out');
+
+    } else {
+      var apiKey = url.param('api_key');
+      var userName = url.param('user_name');
+
+      if (Ember.isEmpty(apiKey)) {
+        apiKey = Cookies.getItem('api_key');
+        userName = Cookies.getItem('user_name');
+      }
+
+      if (!Ember.isEmpty(apiKey)) {
+        var auth = Ember.Object.createWithMixins({
+          apiKey: apiKey,
+          userName: userName,
+          logoutUrl: function() {
+            return window.ENV.api_base_url + '/auth/logout?api_key=' +
+              encodeURIComponent(this.get('apiKey'));
+          }.property('apiKey')
+        });
+        Cookies.setItem('api_key', apiKey);
+        Cookies.setItem('user_name', userName);
+        application.register('auth:main', auth, { instantiate: false });
+        application.inject('controller', 'auth', 'auth:main');
+        application.inject('component', 'auth', 'auth:main');
+        Ember.Logger.debug('App.initializer authentication succeeded: '+auth.get('userName'));
+      }
     }
 
-    if (!Ember.isEmpty(apiKey)) {
-      var auth = Ember.Object.create({
-        apiKey: apiKey,
-        userName: userName
-      });
-      Cookies.setItem('api_key', apiKey);
-      Cookies.setItem('user_name', userName);
-      application.register('auth:main', auth, { instantiate: false });
-      application.inject('controller', 'auth', 'auth:main');
-      application.inject('component', 'auth', 'auth:main');
-      // Clean the auth info querystring from the document's URL.
-      var authenticatedPath = url.attr('path') + '#' + url.attr('fragment');
-      window.history.replaceState("authenticated", "", authenticatedPath);
-
-      Ember.Logger.debug('App.initializer authenticated: '+auth.get('userName'));
-    }
+    // Clean the auth info querystring from the document's URL.
+    var cleanPath = url.attr('path') + '#' + url.attr('fragment');
+    window.history.replaceState("authenticationInitialized", "", cleanPath);
   }
 };
 
